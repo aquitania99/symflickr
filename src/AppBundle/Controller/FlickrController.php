208e425727944b40cbc445b0412e8a2eefcf3917
Sergio Medina <sergiomedina@veromo.com.au>
2017-08-24 00:35:39 +1000

    Reffind Flickr - WIP

--- src/AppBundle/Controller/FlickrController.php
+++ src/AppBundle/Controller/FlickrController.php
@@ -16,43 +16,64 @@ class FlickrController extends Controller
     protected $method;
     protected $format;
 
-    public function indexAction(Request $request)
+    public function indexAction()
+    {
+        $this->apiKey = $this->container->getParameter('flickr_key');
+        $this->apiSecret = $this->container->getParameter('flickr_secret');
+        $this->apiUrl = $this->container->getParameter('flickr_url');
+
+//        $this->keyword = urlencode($request->request->get('keyword'));
+        $this->method = 'flickr.photos.getRecent';
+        $extras = urlencode('tags, description');
+        $resultsPerPage = 10;
+        $url = $this->apiUrl."api_key=".$this->apiKey."&method=".$this->method."&per_page=".$resultsPerPage."&extras=".$extras."format=json";
+        $response = $this->curlCall($url);
+        $photos = $response['photos']['photo'];
+        dump($url);
+        return $this->render('@App/layout.html.twig', array('flickr' => $response));
+    }
+
+    public function searchAction(Request $request)
     {
         if($request->isMethod('POST'))
         {
             $this->apiKey = $this->container->getParameter('flickr_key');
             $this->apiSecret = $this->container->getParameter('flickr_secret');
             $this->apiUrl = $this->container->getParameter('flickr_url');
+            $extras = urlencode('tags, description');
+            $resultsPerPage = 10;
 
             $this->keyword = urlencode($request->request->get('keyword'));
             $this->method = 'flickr.photos.search';
-            $url = $this->apiUrl."api_key=".$this->apiKey."&method=".$this->method."&text=".$this->keyword."&format=php_serial";
+            $url = $this->apiUrl."api_key=".$this->apiKey."&method=".$this->method."&text=".$this->keyword."&extras=".$extras."&per_page=".$resultsPerPage."&format=php_serial";
             $response = $this->curlCall($url);
-
-            return $this->render('@App/layout.html.twig', array('name' => $response));
+            dump($response);
+            return $this->render('@App/layout.html.twig', array('flickr' => $response));
         }
         else {
-            return $this->render('@App/layout.html.twig', array('name' => 'BLAH!'));
+            return $this->render('@App/layout.html.twig', array('flickr' => 'BLAH!'));
         }
-
-    }
-
-    public function searchAction( Request $request)
-    {
-
     }
 
     protected function curlCall($url) {
         $curl = curl_init();
 
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
         $output = curl_exec($curl);
+        $retcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        dump($output);
         curl_close($curl);
-        $result = unserialize ($output);
-        return $result;
+        if ($retcode == 200) {
+            $json = substr( $output, strlen( "jsonFlickrApi(" ), strlen( $output ) - strlen( "jsonFlickrApi(" ) - 1 );
+            $data = json_decode( $json, true );
+            return $data;
+        } else {
+            return null;
+        }
     }
-
 
 }
